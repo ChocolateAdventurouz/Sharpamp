77b3335dc57fdb29af9a39538bc150dc3fe6ecc1
ChocolateAdventurouz <aggeloskarapatakis@hotmail.com>
2023-07-28 07:57:13 +0300

    Added Configuration method to be pre-loaded
    Some people are just blind, and won't check out the wiki page

---
+++ Templates/GeneralPluginTemplate/Plugin/PluginName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+	public class $safeprojectname$ : Daniel15.Sharpamp.GeneralPlugin
+	{
+		public override string Name
+		{
+			get { return "$safeprojectname$"; }
+		}
+
+		public override void Initialize()
+		{
+			// TODO: Put your initialization code here
+			return;
+		}
+	    public override void Config()
+		{
+			// TODO: Put your configuration code here
+			return;
+		}
+	}
+}
